6e859b19711d25437b9b1cb911d21347ed419804
seafooler <seafooler@gmail.com>
2018-01-06 15:09:32 +0800

    Fix a bug

--- benchmark/hyperledger/install.sh
+++ benchmark/hyperledger/install.sh
@@ -35,9 +35,9 @@ sudo apt-get install apt-transport-https ca-certificates curl software-propertie
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
- sudo apt-get update
- sudo apt-get install docker-ce
- sudo usermod -a -G docker $USER
+sudo apt-get update
+sudo apt-get install docker-ce
+sudo usermod -a -G docker $SUDO_USER
 # install dev tools and compilers
 sudo apt-get install -y libsnappy-dev zlib1g-dev libbz2-dev
 sudo apt-get install build-essential
